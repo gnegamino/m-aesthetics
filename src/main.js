d234d20153a0527020dc813e6bb9a5b15fb6330f
Gian Carlo Egamino <gnegamino@gmail.com>
2019-06-03 22:34:22 +0800

    :star: Initial upload

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
