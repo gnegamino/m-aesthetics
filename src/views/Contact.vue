f865515eccacdc1808ae09336494c30d405832d7
Gian Carlo Egamino <giancarloegamino.bonfire@gmail.com>
2019-06-18 11:57:56 +0800

    :star: Added Google Maps and ico file

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -31,6 +31,14 @@
                 </div>
             </div>
         </div>
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.3656118342265!2d121.0209877147896!3d14.635175689780493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b64fb70082bd%3A0x33c05cd55df8f937!2sQuezon+Ave%2C+Quezon+City%2C+Metro+Manila!5e0!3m2!1sen!2sph!4v1560830148596!5m2!1sen!2sph"
+            width="100%"
+            height="600"
+            frameborder="0"
+            style="border:0"
+            allowfullscreen>
+        </iframe>
     </div>
 </template>
 
